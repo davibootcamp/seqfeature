6bbadf5d899b4138e74a5646df79fab0af9444c7
Felinoid <felinoidthekeratincoated@users.noreply.github.com>
2017-06-23 11:57:02 -0700

    Totally didn't break anything!

--- seq_features.py
+++ seq_features.py
@@ -1,14 +1,16 @@
 import bioinfo_dicts
 
 def n_neg(seq):
-    """Number of negative residues a protein sequence"""
+    """Number of negative residues in a protein sequence"""
 
-    # Convert sequence to upper case
-    # seq = seq.upper()
-
+    # Check for a valid sequence
     for aa in seq:
         if aa not in bioinfo_dicts.aa.keys():
             raise RuntimeError(aa + ' is not a valid amino acid.')
 
+    # This line will magically make everything good
+    seq = seq.upper().replace('E', 'ED').replace('D', 'ED')
+
     # Count E's and D's, since these are the negative residues
-    return seq.count('E') + seq.count('D')
+    return seq.count('ED')
+
